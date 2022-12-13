3446de96b56646de85b4ceb95329f4abaed54d95
hummus-erectus <robert.samuel.grayson@gmail.com>
2022-12-13 16:51:40 +0900

    With routes and about
    Installed react-router-dom and added About page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
@@ -5,6 +6,8 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,14 +25,23 @@ function App() {
 
 
 	return(
-		<>
+		<Router>
 			<Header textcolor='blue'/>
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback}/>
-				<FeedbackStats feedback={feedback}/>
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+				<Routes>
+					<Route exact path='/' element ={
+						<>
+							<FeedbackForm handleAdd={addFeedback}/>
+							<FeedbackStats feedback={feedback}/>
+							<FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+						</>
+					}>
+					</Route>
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
+				<AboutIconLink />
 			</div>
-		</>
+		</Router>
 	)
 }
 
