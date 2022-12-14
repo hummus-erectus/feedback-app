49b09ef081fbb3d4611aba9f8d5fea224be1ab0f
hummus-erectus <robert.samuel.grayson@gmail.com>
2022-12-14 13:07:11 +0900

    Build v.1
    Refactored with useContext, added editing

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,11 @@
 import {motion, AnimatePresence} from 'framer-motion'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+
     if( !feedback || feedback.length === 0 ) {
         return <p>No Feedback Yet</p>
     }
@@ -17,21 +21,12 @@ function FeedbackList({feedback, handleDelete}) {
                         exit={{opacity:0}}
                         layout
                     >
-                        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+                        <FeedbackItem key={item.id} item={item} />
                     </motion.div>
                 ))}
             </AnimatePresence>
         </div>
     )
-
-
-    // return (
-    //     <div className="feedback-list">
-    //         {feedback.map((item) => (
-    //             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-    //         ))}
-    //     </div>
-    // )
 }
 
 export default FeedbackList
